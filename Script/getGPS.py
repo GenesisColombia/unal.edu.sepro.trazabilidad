ed71a2ddbfc067a18016a4042ecb04bf40f97f96
jrmejiaa <jrmejiaa@unal.edu.co>
2016-12-12 23:23:03 +0000

    Prueba final GPS, funciona con antena activa, creacion de un archivo power para inicializar el modulo FONA808

--- Script/getGPS.py
+++ Script/getGPS.py
@@ -5,12 +5,9 @@ from RPi_Driver import Send,KeepGPS
 ser = serial.Serial('/dev/ttyAMA0',115200,timeout=2);
 
 Send(ser,'AT\n','OK\r\n',1);
-Send(ser,'AT+CGNSPWR=1\n','OK\r\n',1);
-sleep(1);
 Send(ser,'AT+CGNSSEQ="RMC"\n','OK\r\n',1);
 # Keep information of GPS
 Date,Lon,Lat = KeepGPS(ser);
-Send(ser,'AT+CGNSPWR=0\n','OK\r\n',1);
 print Date;
 print Lon;
 print Lat;
