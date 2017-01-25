9d6fb57171a8f4320dccd9951be7d36b2acbef44
Julian L <jflatorreo@unal.edu.co>
2017-01-25 13:56:40 -0500

    Loop

--- Script/loop.py
+++ Script/loop.py
@@ -9,11 +9,6 @@ while (1):
 	
 	Date,Lon,Lat,Alt =getGPS.write()
 	Date=datetime.datetime.strptime(str(int(float(Date))),'%Y%m%d%H%M%S')
-	print "Date,Lon,Lat,Alt",Date,Lon,Lat,Alt
-	Temp=0
-	Hum=0
-	print "reset...",Temp,Hum
 	Hum,Temp=getTempHum.write()
 	post.send(Date,Lon,Lat,Alt,Temp,Hum)
-	print "TemperaHumedad...",Temp,Hum
-	time.sleep(3)
+	time.sleep(30)
